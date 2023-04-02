798c8c1806a4961834853bb16be4e63739b63aa5
Luke O.S <loshrader@gmail.com>
2023-04-01 21:54:06 -0400

    a

--- assets/CoreModule.py
+++ assets/CoreModule.py
@@ -1,26 +1,27 @@
 import time
-import firebase_admin
-from datetime import datetime
-from firebase_admin import credentials
-from firebase_admin import firestore
+#import firebase_admin
+#from datetime import datetime
+#from firebase_admin import credentials
+#from firebase_admin import firestore
 
 #Firebase
-cred = credentials.Certificate("./assets/secure/firebaseKey.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-collection = db.collection('core')
+#cred = credentials.Certificate("./assets/secure/firebaseKey.json")
+#firebase_admin.initialize_app(cred)
+#db = firestore.client()
+#collection = db.collection('core')
 
 while True:
 
-    now = datetime.now()
+   # now = datetime.now()
 
-    current_time_H = now.strftime("%H")
-    current_time_M = now.strftime("%M")
+   # current_time_H = now.strftime("%H")
+    #current_time_M = now.strftime("%M")
+   time.sleep(5)
 
-    time.sleep(5)
+   print("HI!!")
 
-    res = collection.document('values').get().to_dict()
-    print(res['pc_enabled'])
+   # res = collection.document('values').get().to_dict()
+    #print(res['pc_enabled'])
 
 
 
