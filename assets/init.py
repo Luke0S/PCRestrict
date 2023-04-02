c26207b71d49c5b108912847c868fdaa5c60b6a1
Luke O.S <loshrader@gmail.com>
2023-04-02 18:57:48 -0400

    uu

--- assets/init.py
+++ assets/init.py
@@ -3,12 +3,16 @@ import json
 import os
 
 def add_process():
-    pid = os.getpid()
-    with open("./values/global.json", 'r+') as file:
-        file_data = json.load(file)
-        file_data["pid"].append(pid)
-        file.seek(0)
-        json.dump(file_data, file, indent=4)
+    while not os.path.exists("./values/global.json"):
+        time.sleep(1)
+
+    if os.path.isfile("./values/global.json"):
+        pid = os.getpid()
+        with open("./values/global.json", 'r+') as file:
+            file_data = json.load(file)
+            file_data["pid"].append(pid)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
 
 add_process()
 
